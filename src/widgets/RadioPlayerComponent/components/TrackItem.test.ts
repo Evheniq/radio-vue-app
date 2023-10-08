49d40cef3919338fd34a2f488012caba4e3bb2ee
Evheniq <qundola@gmail.com>
2023-10-09 01:25:44 +0300

    Use reusable mock for Track data

--- src/widgets/RadioPlayerComponent/components/TrackItem.test.ts
+++ src/widgets/RadioPlayerComponent/components/TrackItem.test.ts
@@ -2,20 +2,21 @@ import { expect, test, describe } from "vitest";
 import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import TrackItem from './TrackItem.vue';
+import {ITrack} from "../types/track.ts";
+
+// Mock data
+const trackDataMock: ITrack = {
+    title: 'Test Track',
+    artist: 'Test Artist',
+    album: 'Test Album',
+    duration: '00:03:30',
+    time: new Date().toISOString(),
+    imageUrl: 'https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/5f/85/bb/5f85bbf3-de53-a9e2-4e81-3d07080eb080/mzi.mnsosfnc.jpg/450x450bb.jpg',
+    status: 'playing',
+};
 
 describe("TrackItem.vue tests", () => {
     test('displays track data correctly', async () => {
-        // Mock data
-        const trackDataMock = {
-            title: 'Test Track',
-            artist: 'Test Artist',
-            album: 'Test Album',
-            duration: '00:03:30',
-            time: new Date().toISOString(),
-            imageUrl: 'https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/5f/85/bb/5f85bbf3-de53-a9e2-4e81-3d07080eb080/mzi.mnsosfnc.jpg/450x450bb.jpg',
-            status: 'playing',
-        };
-
         // Mount component
         const wrapper = mount(TrackItem, {
             props: { trackData: trackDataMock, isPlaying: true },
@@ -29,17 +30,6 @@ describe("TrackItem.vue tests", () => {
     });
 
     test('handles image error', async () => {
-        // Mock data
-        const trackDataMock = {
-            title: 'Test Track',
-            artist: 'Test Artist',
-            album: 'Test Album',
-            duration: '00:03:30',
-            time: new Date().toISOString(),
-            imageUrl: undefined,
-            status: 'playing',
-        };
-
         const wrapper = mount(TrackItem, {
             props: { trackData: trackDataMock, isPlaying: true },
         });
